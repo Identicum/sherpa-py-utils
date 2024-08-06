63eb1a4147e24fb8ea445f2c27a18d2944cd4163
Gustavo J Gallardo <ggallard@identicum.com>
2024-08-06 16:04:53 -0300

    Update test.py

--- docker/test.py
+++ docker/test.py
@@ -19,15 +19,21 @@ def run(logger, properties):
 	logger.info("{} starting.".format(os.path.basename(__file__)))
 
 	ip_address = "samba"
-	user_dn = "cn=administrator,cn=users,dc=sherpa-demo,dc=com"
+	base_dn = "dc=sherpa-demo,dc=com"
+	user_dn = "cn=administrator,cn=users,{}".format(base_dn)
 	user_password = "Sherpa.2024"
 	ldap = LDAP(ip_address=ip_address, user_dn=user_dn, user_password=user_password, logger=logger)
+	users_base_dn = "ou=sherpa_users,{}".format(base_dn)
+	groups_base_dn = "ou=sherpa_groups,{}".format(base_dn)
 
-	ldap.create_ad_ou("dc=sherpa-demo,dc=com", "meta")
-	ldap.create_ad_ou("ou=meta,dc=sherpa-demo,dc=com", "users")
-	ldap.create_ad_ou("ou=meta,dc=sherpa-demo,dc=com", "groups")
-
-	ldap.create_ad_user("ou=users,ou=meta,dc=sherpa-demo,dc=com", "testuser", "testPassword.2024", "testuser@sherpa-demo.com", "Test", "User")
+	ldap.create_ad_ou(base_dn, "sherpa_users")
+	ldap.create_ad_ou(base_dn, "sherpa_groups")
+	ldap.create_ad_user(users_base_dn, "testuser1", "testPassword.2024", "testuser1@sherpa-demo.com", "Test", "User1")
+	ldap.create_ad_user(users_base_dn, "testuser2", "testPassword.2024", "testuser2@sherpa-demo.com", "Test", "User2")
+	group_members = []
+	group_members.append(("cn=testuser1,{}".format(users_base_dn)).encode())
+	group_members.append(("cn=testuser2,{}".format(users_base_dn)).encode())
+	ldap.create_ad_group(groups_base_dn, "testgroup", group_members)
 
 
 if __name__ == "__main__":
