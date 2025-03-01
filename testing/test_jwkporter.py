4c32fa8a1c4db95d696f6a72eaa7f14724764d8c
Luciano Agustin Crocco <lucianoacrocco@gmail.com>
2025-03-01 01:06:24 -0300

    Class execution-agonstic, added create method

--- testing/test_jwkporter.py
+++ testing/test_jwkporter.py
@@ -16,41 +16,49 @@ from sherpa.utils.basics import Properties
 from sherpa.utils.basics import Logger
 
 class JWKPorter:
-	def __init__(self, properties, logger, oidc_client: OIDCClient, jwkporter_base_url: str):
-		self.oidc_client = oidc_client
-		self.properties = properties
+	def __init__(self, logger, jwkporter_base_url, idp_url):
 		self.logger = logger
+		self._oidc_client = None
 		self.jwkporter_base_url = jwkporter_base_url
+		self.idp_url = idp_url
 
-	def _obtain_access_token(self):
-		client_id = self.properties.get("jwkporter_client_id")
-		client_secret = self.properties.get("jwkporter_client_secret")
+	@property
+	def oidc_client(self):
+		if self._oidc_client is None:
+			self.logger.debug("Creating an OIDC Client for connections to IdP: {}".format(self.idp_url))
+			self._oidc_client = OIDCClient(self.idp_url,self.logger, True)
+		return self._oidc_client
+
+	def _obtain_access_token(self, client_id, client_secret):
+		client_id = client_id
+		client_secret = client_secret
 		credentials = f"{client_id}:{client_secret}".encode()
 		b64_credentials = base64.b64encode(credentials).decode()
 		credentials = self.oidc_client.do_client_credentials(b64_credentials, scope="openid jwksporter:sign jwksporter:crud")
 		access_token = self.oidc_client.extract_access_token(credentials)
 		return access_token
 
-	def sign(self, args):
-		access_token = self._obtain_access_token()
+	def sign(self, client_id, client_secret, exp, features, customer, product, kid):
+		self.logger.debug("Running method to sign JWT against JWK with public key {}".format("8388bb6b-4d8e-4be3-a5d4-080523a75e9b"))
+		access_token = self._obtain_access_token(client_id, client_secret)
 		endpoint_url = self.jwkporter_base_url + '/token/sign'
 		iat = int(time.time())
 
 		try:
-			exp = int(datetime.strptime(args.exp, "%Y%m%d").timestamp())
+			exp = int(datetime.strptime(exp, "%Y%m%d").timestamp())
 		except ValueError:
 			raise ValueError("Invalid expiration format. Please use YYYYMMDD.")
 
-		features = [feature.strip() for feature in args.features.split(",")]
+		features = [feature.strip() for feature in features.split(",")]
 		if not features:
 			raise ValueError("Features list cannot be empty.")
 		payload = json.dumps({
-			"kid": "8388bb6b-4d8e-4be3-a5d4-080523a75e9b",
+			"kid": kid,
 			"payload": {
 				"iat": iat,
 				"exp": exp,
-				"customer": args.customer,
-				"product": args.product,
+				"customer": customer,
+				"product": product,
 				"features": features
 			}
 		})
@@ -62,12 +70,26 @@ class JWKPorter:
 		response = requests.post(url=endpoint_url, headers=headers, data=payload)
 		if response.status_code == 200:
 			self.logger.debug("signed_payload: {}", response.content)
+			return response.content
 		else:
 			self.logger.debug(response)
 			raise Exception("Failed to sign payload: {} {}".format(response.status_code, response.text))
 
-	def create(self, logger, properties, payload):
-		logger.debug("Payload: {}", payload)
+	def create(self, client_id, client_secret):
+		self.logger.debug("Creating a JWK Key in JWKPortainer instance")
+		access_token = self._obtain_access_token(client_id, client_secret)
+		endpoint_url = self.jwkporter_base_url + '/jwks/manage'
+		headers = {
+			"Authorization": "Bearer {}".format(access_token),
+			"Content-Type": "application/json"
+		}
+		response = requests.post(url=endpoint_url, headers=headers)
+		if response.status_code == 200:
+			self.logger.debug("jwk: {}", response.content)
+			return response.content
+		else:
+			self.logger.debug(response)
+			raise Exception("Failed to create a JWK: {} {}".format(response.status_code, response.text))
 
 	def verify(self, logger, properties, payload):
 		logger.debug("Payload: {}", payload)
@@ -89,17 +111,24 @@ def main(arguments):
 
 def run(logger, properties, args):
 	logger.info("{} starting.".format(os.path.basename(__file__)))
-	logger.debug("Obtaining idp url and jwkporter url from properties")
+	exp = args.exp
+	features = args.features
+	customer = args.customer
+	product = args.product
+	logger.debug("Obtaining client_id, client_secret, jwkporter base url and idp base url")
+	client_id = properties.get("jwkporter_client_id")
+	client_secret = properties.get("jwkporter_client_secret")
+	jwkporter_base_url = properties.get("jwkporter_base_url")
 	idp_url = properties.get("idp_url")
-	jwkporter_url = properties.get("jwkporter_base_url")
-	logger.info("Obtained {} as idp_url and {} as jwkporter_base_url".format(idp_url, jwkporter_url))
 
-	logger.debug("Creating OIDCClient instance")
-	oidc = OIDCClient(idp_url, logger, True)
 	logger.debug("Creating JWKPorter instance")
-	jwkporter = JWKPorter(properties=properties, logger=logger, oidc_client=oidc, jwkporter_base_url=jwkporter_url)
-	logger.debug("Running JWKPorter sign method")
-	jwkporter.sign(args)
+	jwkporter = JWKPorter(logger, jwkporter_base_url, idp_url)
+
+	jwk = jwkporter.create(client_id, client_secret)
+	data = json.loads(jwk.decode('utf-8'))
+	kid = data.get("kid")
+
+	jwkporter.sign(client_id, client_secret, exp, features,customer, product, kid)
 
 	logger.debug("run finished successfully")
 
