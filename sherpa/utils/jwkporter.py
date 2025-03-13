289fa2a4075ec4332913cfd31cf47a14aaaac1b7
Martín Zielony <mzielony@identicum.com>
2025-03-13 15:25:02 -0300

    JWKPorter utility fixes/tweaks and docstrings

--- sherpa/utils/jwkporter.py
+++ sherpa/utils/jwkporter.py
@@ -6,17 +6,31 @@
 #
 
 import base64
-from datetime import datetime
 
 import requests
 import json
-import time
 
 from sherpa.utils.clients import OIDCClient
 
 JWKS_SIGN_SCOPE = "jwksporter:sign"
 JWKS_CRUD_SCOPE = "jwksporter:crud"
 class JWKPorter:
+	"""
+	JWKPorter Class is a wrapper for the JWKPorter REST API, used for the following purpouses
+	- Signing JSONs with a public key
+	- Key Creation
+	- JWT Verification
+	- Public Key Retrieval
+
+	Attributes:
+		logger: Logger class instance
+		oidc_client
+		jwkporter_base_url: Base URL of the JWKPorter REST API
+		idp_url
+		client_id
+		client_secret
+	"""
+
 	def __init__(self, logger, jwkporter_base_url, idp_url, client_id, client_secret):
 		self.logger = logger
 		self.oidc_client = OIDCClient(idp_url, logger, True)
@@ -26,6 +40,12 @@ class JWKPorter:
 		self.client_secret = client_secret
 
 	def _obtain_access_token(self, scope = None):
+		"""
+		Retrieves and returns Access Token from the OIDC Client
+
+		Args:
+			scope (Defaults to 'oidc')
+		"""
 		credentials = f"{self.client_id}:{self.client_secret}".encode()
 		b64_credentials = base64.b64encode(credentials).decode()
 		try:
@@ -37,14 +57,27 @@ class JWKPorter:
 		return access_token
 
 	def sign_json(self, json_payload, kid):
+		"""
+		Receives a JSON Payload, signs and then returns it
+
+		Args:
+			json_payload
+			kid: Key ID to sign with
+		"""
 		self.logger.debug("Running method to sign JWT against JWK with public key {}".format(kid))
-		access_token = self._obtain_access_token(self.client_id, self.client_secret, JWKS_SIGN_SCOPE)
+		access_token = self._obtain_access_token(JWKS_SIGN_SCOPE)
 		endpoint_url = self.jwkporter_base_url + '/token/sign'
 		
 		headers = {
 			"Authorization": "Bearer {}".format(access_token),
 			"Content-Type": "application/json"
 		}
+
+		request_payload = json.dumps({
+			"kid": kid,
+			"payload": json_payload
+		})
+
 		self.logger.debug("Calling url: {} with headers: {} and request_payload: {}", endpoint_url, headers, request_payload)
 		response = requests.post(url=endpoint_url, headers=headers, data=request_payload)
 		if response.status_code == 200:
@@ -55,8 +88,11 @@ class JWKPorter:
 			raise Exception("Failed to sign payload: {} {}".format(response.status_code, response.text))
 
 	def create_key(self):
-		self.logger.debug("Creating a JWK Key in JWKPortainer instance")
-		access_token = self._obtain_access_token(self.client_id, self.client_secret, JWKS_CRUD_SCOPE)
+		"""
+		Creates and returns a JWK Key
+		"""
+		self.logger.debug("Creating a JWK Key in JWKPorter instance")
+		access_token = self._obtain_access_token(JWKS_CRUD_SCOPE)
 		endpoint_url = self.jwkporter_base_url + '/jwks/manage'
 		headers = {
 			"Authorization": "Bearer {}".format(access_token),
@@ -71,8 +107,15 @@ class JWKPorter:
 			raise Exception("Failed to create a JWK: {} {}".format(response.status_code, response.text))
 
 	def verify_jwt(self, kid, signed_jwt):
+		"""
+		Verifies the signature in a JWT
+
+		Args:
+			kid: Key ID The JWT should be signed with
+			signed_jwt
+		"""
 		self.logger.debug("Verifying signed JWT {} with kid {}".format(signed_jwt, kid))
-		access_token = self._obtain_access_token(self.client_id, self.client_secret)
+		access_token = self._obtain_access_token()
 		endpoint_url = self.jwkporter_base_url + '/token/verify'
 		headers = {
 			"Authorization": "Bearer {}".format(access_token),
@@ -91,12 +134,13 @@ class JWKPorter:
 			raise Exception("Failed to verify signed jwt: {} {}".format(response.status_code, response.text))
 	
 	def get_keys(self):
+		"""
+		Retrieves and returns all Public Keys
+		"""
 		self.logger.debug("Fetching Public Keys")
-		access_token = self._obtain_access_token(self.client_id, self.client_secret)
 		endpoint_url = self.jwkporter_base_url + '/jwks'
 
 		headers = {
-			"Authorization": "Bearer {}".format(access_token),
 			"Content-Type": "application/json"
 		}
 
