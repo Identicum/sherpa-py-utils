b3fff359aae0ea9d05a818dcaa2e639f3cfa21d2
Martín Zielony <mzielony@identicum.com>
2025-03-13 15:25:29 -0300

    licence.py for licence JSON creation

--- sherpa/utils/licence.py
+++ sherpa/utils/licence.py
@@ -5,18 +5,34 @@
 #  Martín Zielony - mzielony@identicum.com
 #
 
-import json
+from datetime import datetime
+
 import time
 
-def build_license_json(expiration, features, customer, product):
-    """Builds and returns a JSON containing License info and IAT timestamp."""
+def build_license_json(product, customer, expiration, features):
+    """
+    Builds and returns a JSON containing License info and IAT timestamp.
+    Args:
+        product: Product Name.
+        customer: Name of the customer the licence's for.
+        expiration: Licence Expiration Date (YYYYMMDD).
+        features: Single String listing features separated by commas.
+    """
     iat = int(time.time())
-    return json.dumps(
-        {
+    
+    try:
+        exp = int(datetime.strptime(expiration, "%Y%m%d").timestamp())
+    except ValueError:
+        raise ValueError("Invalid expiration format. Please use YYYYMMDD.")
+    
+    features_array = [feature.strip() for feature in features.split(",")]
+    if not features_array:
+        raise ValueError("Features list cannot be empty.")
+    
+    return {
             "iat": iat,
-            "expiration": expiration,
-            "features": features,
-            "customer": customer,
             "product": product,
+            "customer": customer,
+            "expiration": exp,
+            "features": features_array,
         }
-    )
